bf0465c996dc159ab4c72fdb04636cc8da6a426b
David <David@David-PC>
2017-04-14 10:52:42 +0800

    添加了注释

--- src/com/coretek/avt/executor/Launcher.java
+++ src/com/coretek/avt/executor/Launcher.java
@@ -9,13 +9,17 @@ package com.coretek.avt.executor;
  */
 public class Launcher
 {
-
 	/**
-	 * @param args
+	 * 
+	 * @param args 命令行参数：
+	 * 1.mode=debug/run:运行模型，执行器支持调试模式和运行模式
+	 * 2.endian=big/little:大小端设置
+	 * 3.beginMsg=uuid:开始消息的UUID
+	 * 4.endMsg=uuid:结束消息的UUID
 	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		
 
 	}
 
