7c16cdd2112a06d15a1be8a06f1dd4b3a8896677
David <David@David-PC>
2017-04-14 10:40:49 +0800

    初始导入

---
+++ src/com/coretek/avt/executor/Launcher.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.coretek.avt.executor;
+
+/**
+ * @author David
+ *
+ */
+public class Launcher
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
